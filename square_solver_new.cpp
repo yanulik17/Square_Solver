9a262ca8d1374b63f8e638bf96ee1f5c41cd7816
yanulik17 <kuznetsova.iaa@phystech.edu>
2025-08-22 16:42:45 +0300

    Add files via upload

---
+++ square_solver_new.cpp
@@ -0,0 +1,84 @@
+#include <TXLib.h>
+#include <stdio.h>
+
+int Solve_Square (double a, double b, double c);
+int Solve_Linear (double a, double b, double c);
+int Input (void);
+int Output (void);
+
+// функция ввода коэффициентов -------------------------------------------
+
+int Input (void) {
+
+    struct
+
+    double a = 0, b = 0, c = 0;
+    bool flag = 1;  // флаг для отсева нечисленных вводов
+
+    printf("Введите коэффициенты квадратного уравнения каждый в новой строчке в порядке a, b, c:\n");
+
+    while (scanf("%lg", &a) + scanf("%lg", &b) + scanf("%lg", &c) == 3) {
+        flag = 1;
+    }
+
+    if (flag == 0) {
+        printf("Некорректный ввод данных.");
+    }
+    return ;
+}
+
+// функция вывода ответов ------------------------------------------------
+
+int Output (void) {
+
+}
+
+// функция решения линейного уравнения -----------------------------------
+
+int Solve_Linear (double a, double b, double c)
+{
+    double x;
+    x = - (c / b);
+    printf("%lg", x);
+    return 0;
+}
+
+// функция решения квадратного уравнения ----------------------------------
+
+int Solve_Square (double a, double b, double c)
+{
+    double D = 0, x_1 = 0, x_2 = 0, x = 0, delta = 1e-9;
+    D = b * b - 4 * a * c;
+
+    if (D > 0 + delta) { // дискриминант больше нуля
+        printf("Уравнение имеет два различных действительных корня: ");   // switch
+                                                                            // case, прочитать про enum
+        x_1 = (-b - sqrt(D)) / (2 * a);
+        printf("%lg", x_1);
+        printf(" и ");
+        x_2 = (-b + sqrt(D)) / (2 * a);
+        printf("%lg", x_2);
+        return 0;
+    }
+
+    else if (D < 0 - delta) {  // дискриминант меньше нуля
+        printf("Уравнение не имеет решений в действительных числах.\n");
+        return 0;
+    }
+
+    else {     // дискриминант ноль
+        printf("Уравнение имеет ровно один действительный корень: ");
+        x = (-b - sqrt(D)) / (2 * a);
+        printf("%lg", x);
+        return 0;
+        }
+}
+
+//----------------------------------------------------------------------
+
+int main (void)
+{
+    printf(Input());
+
+    return 0;
+}
